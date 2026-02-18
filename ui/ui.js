422182068c98c0ce86e0ef8feee49e5b653a7d08
ryker-netizen <rykerdiblin@gmail.com>
2026-02-18 19:01:52 +0700

    Update ui.js

--- ui/ui.js
+++ ui/ui.js
@@ -1,35 +1,33 @@
-export class UI{
-  constructor(engine){
-    this.engine = engine;
+export function initUI(engine){
 
-    const chaos = document.getElementById("chaos");
-    const pixel = document.getElementById("pixel");
+  const chaos = document.getElementById("chaos");
+  const pixel = document.getElementById("pixel");
 
-    chaos.oninput = e=>{
-      engine.params.destruction = parseFloat(e.target.value);
+  chaos.oninput = e=>{
+    engine.params.destruction = parseFloat(e.target.value);
+  };
+
+  pixel.oninput = e=>{
+    engine.params.pixel = parseFloat(e.target.value);
+  };
+
+  // загрузка видео
+  document.getElementById("load").onclick = ()=>{
+    const input = document.createElement("input");
+    input.type = "file";
+    input.accept = "video/*";
+
+    input.onchange = e=>{
+      const file = e.target.files[0];
+      engine.video.load(file);
     };
 
-    pixel.oninput = e=>{
-      engine.params.pixel = parseFloat(e.target.value);
-    };
+    input.click();
+  };
 
-    // загрузка видео
-    document.getElementById("load").onclick = ()=>{
-      const input = document.createElement("input");
-      input.type = "file";
-      input.accept = "video/*";
+  // запись
+  document.getElementById("rec").onclick = ()=>{
+    engine.recorder.toggle();
+  };
 
-      input.onchange = e=>{
-        const file = e.target.files[0];
-        engine.video.load(file);
-      };
-
-      input.click();
-    };
-
-    // запись
-    document.getElementById("rec").onclick = ()=>{
-      engine.recorder.toggle();
-    };
-  }
 }
