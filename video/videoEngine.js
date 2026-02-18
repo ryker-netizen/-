0e55bde0dbbcdde8e10153c4e9419b0010346aa9
ryker-netizen <rykerdiblin@gmail.com>
2026-02-18 17:33:31 +0700

    Create videoEngine.js

---
+++ video/videoEngine.js
@@ -0,0 +1,25 @@
+export class VideoEngine{
+  constructor(canvas){
+    this.canvas = canvas;
+    this.ctx = canvas.getContext("2d");
+
+    this.video = document.createElement("video");
+    this.video.loop = true;
+    this.video.muted = true;
+  }
+
+  load(file){
+    this.video.src = URL.createObjectURL(file);
+    this.video.play();
+  }
+
+  drawBase(){
+    if(this.video.readyState>=2){
+      this.ctx.drawImage(this.video,0,0,this.canvas.width,this.canvas.height);
+    }
+  }
+
+  getFrame(){
+    return this.ctx.getImageData(0,0,this.canvas.width,this.canvas.height);
+  }
+}
